b02027e18e4a104db03cc1194b75fd783d68b88a
Feyereisen Vladislav <feyereisen.vladislav@gmail.com>
2024-10-26 20:06:48 +0200

    More clean
    cleaner code, no repetition

--- is_it_primenumber.py
+++ is_it_primenumber.py
@@ -3,8 +3,7 @@ import math
 number = 432668237
 divider = [2,3,5,7,13,17]
 
-
-def prime1(n):
+def find_prime(n):
   global divider
   square = math.sqrt(n)
   for i in range(len(divider)):
@@ -13,29 +12,24 @@ def prime1(n):
     else:
       if n % divider[i] == 0:
         return "not prime"
+  return "find more primedivider please"
 
 def prime(n):
   global divider
   square = math.sqrt(n)
-  if n == 1:
-    return "not prime"
-  for i in range(len(divider)):
-    if divider[i-1] < square and divider[i] > square:
-      return "prime"
-    else:
-      if n % divider[i] == 0:
-        return "not prime"
-    
-  index = 1
-  while divider[len(divider)-1] < square:
-    next_prime = divider[len(divider)-1] + index
-    if prime1(next_prime) == "prime":
-      divider.append(next_prime)
-      index = 1
-      if n % next_prime == 0:
-        return "not prime"
-    else:
-      index +=1
-  return "prime"
+  if find_prime(n) == "find more primedivider please":
+    index = 1
+    while divider[len(divider)-1] < square:
+      next_prime = divider[len(divider)-1] + index
+      if find_prime(next_prime) == "prime":
+        divider.append(next_prime)
+        index = 1
+        if n % next_prime == 0:
+          return "not prime"
+      else:
+        index +=1
+    return "prime"
+  else:
+    return find_prime(n)
     
 print(f"The number {number} is {prime(number)}.")
