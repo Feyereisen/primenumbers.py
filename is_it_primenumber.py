ac0569d6c510846cdcf265eafb182c59b443cd53
Feyereisen Vladislav <feyereisen.vladislav@gmail.com>
2024-10-26 18:33:30 +0200

    replacing abs with sqrt

--- is_it_primenumber.py
+++ is_it_primenumber.py
@@ -2,24 +2,27 @@ import math
 
 n = 57
 divider = [2,3,5,7,13,17]
+square = math.sqrt(n)
 
 def prime1(n):
+  global square
   for i in range(len(divider)):
-    if n[i-1] < abs(n) and n[i] > abs(n):
+    if n[i-1] < square and n[i] > square:
       return "prime"
     else:
       if n % n[i] == 0:
         return "not prime"
 
 def prime(n):
+  global square
   for i in range(len(divider)):
-    if n[i-1] < abs(n) and n[i] > abs(n):
+    if n[i-1] < square and n[i] > square:
       return "prime"
     else:
       if n % n[i] == 0:
         return "not prime"
   
-  while n[len(n)] < abs(n):
+  while n[len(n)] < square:
     index = 1
     next_prime = n[len(n)] + index
     if prime1(next_prime) == "prime":
