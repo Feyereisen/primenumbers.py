22c7fa0fc50ee441003c8c935b73615910943a9c
Feyereisen Vladislav <feyereisen.vladislav@gmail.com>
2024-10-26 19:26:19 +0200

    while logic
    the index needs to be outside
    it is divider[len(divider)-1], because it starts counting from 0

--- is_it_primenumber.py
+++ is_it_primenumber.py
@@ -1,12 +1,12 @@
 import math
 
-number = 1
+number = 432668237
 divider = [2,3,5,7,13,17]
-square = math.sqrt(number)
+
 
 def prime1(n):
-  global square
   global divider
+  square = math.sqrt(n)
   for i in range(len(divider)):
     if divider[i-1] < square and divider[i] > square:
       return "prime"
@@ -15,27 +15,27 @@ def prime1(n):
         return "not prime"
 
 def prime(n):
-  global square
   global divider
+  square = math.sqrt(n)
   if n == 1:
     return "not prime"
   for i in range(len(divider)):
-    print(divider[i])
     if divider[i-1] < square and divider[i] > square:
       return "prime"
     else:
       if n % divider[i] == 0:
         return "not prime"
-  
+    
+  index = 1
   while divider[len(divider)-1] < square:
-    index = 1
-    next_prime = divider[len(n)] + index
+    next_prime = divider[len(divider)-1] + index
     if prime1(next_prime) == "prime":
       divider.append(next_prime)
       if n % next_prime == 0:
         return "not prime"
     else:
       index +=1
+    print(divider[len(divider)-1])
   return "prime"
     
 print(f"The number {number} is {prime(number)}.")
